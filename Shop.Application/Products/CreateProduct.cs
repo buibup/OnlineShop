8b392fcae874cf20da581a2c1ec2de6f5a72ec1b
buibup <b5209194@gmail.com>
2019-03-11 00:29:06 +0700

    completed Ep.6 - View Models

---
+++ Shop.Application/Products/CreateProduct.cs
@@ -0,0 +1,25 @@
+﻿using Shop.Database;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Products
+{
+    public class CreateProduct
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateProduct(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Do(Product product) 
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Shop.UI/Pages/Index.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shop.Application.Products;
+using Shop.Database;
+using Shop.Domain.Models;
+
+namespace Shop.UI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _ctx;
+
+        public IndexModel(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        [BindProperty]
+        public ProductViewModel Product { get; set; }
+        public class ProductViewModel
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Value { get; set; }
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var product = new Product
+            {
+                Name = Product.Name,
+                Description = Product.Description,
+                Value = Product.Value
+            };
+            await new CreateProduct(_ctx).Do(product);
+
+            return RedirectToPage("Index");
+        }
+    }
+}
